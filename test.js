bc196b63b7219ade809462944e55479376b94aa9
pj <pj_panjian@163.com>
2018-01-23 13:54:38 +0800

    1

---
+++ test.js
@@ -0,0 +1,10 @@
+var koa = require('koa')
+var app = new koa()
+var fs = require('co-fs')
+
+app.use(function * () {
+    let data = yield fs.readFile('./json/area.json', 'utf8')
+    this.body = data;
+})
+
+app.listen(3000)
